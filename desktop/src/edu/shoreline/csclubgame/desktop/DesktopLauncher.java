8197d3adc7560a41b49ffb2b2b9e69ef472f510c
Kneelawk <jedidiah.pommert@gmail.com>
2019-11-24 22:32:24 -0800

    Create libGDX project

---
+++ desktop/src/edu/shoreline/csclubgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.shoreline.csclubgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.shoreline.csclubgame.CSClubGameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CSClubGameMain(), config);
+	}
+}
